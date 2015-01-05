928a428a79e574edc9e00416335eccba139dffdd
Mark Palmeri <mlp6@duke.edu>
2015-01-05 15:53:10 -0500

    change csv lists to python sets

--- nn27.py
+++ nn27.py
@@ -1,27 +1,35 @@
-1p,2p,2a,1a,7p,7a,3p
-2p,1p,1a,2a,4p
-3p,4p,3a,9p,5p,1p,
-4p,4a,3p,3a,2p,6p,
-5p,6p,11p,5a,15as,3p,6p
-6p,5p,6a,5a,4p,,
-7p,1p,8p,7a,8a,1a,9p
-8p,7p,8a,10p,7a,
-9p,10p,3p,9a,7a,11p,
-10p,9p,10a,9a,8p,12p,
-11p,12p,5p,11a,15as,12a,9p
-12p,11p,12a,11a,10p,
-1a,13as,2a,7a,3a,
-2a,1a,13as,2p,4a,
-3a,5a,4p,3p,14as,5a,1p
-4a,3a,14as,4p,2a,6a,
-5a,15as,6a,3a,5p,
-6a,5a,15as,6p,4a,
-7a,1a,13as,8a,7p,9a,
-8a,8p,7a,13as,10a,
-9a,10a,14as,7a,11a
-10a,10p,8a,12a,9a
-11a,12a,15as,12p,9a
-12a,11a,15as,12p,10a
-13as,1a,7a,8a,2a,14as,
-14as,3a,9a,4a,10a,13as,15a
-15as,6a,12a,5a,11a,14as
+'''
+nn27.py - create nearest neighbor sets for the 27 prostate regions
+'''
+
+__author__ = 'Mark L. Palmeri'
+__email__ = 'mlp6@duke.edu'
+__date__ = '2014-01-05'
+
+r1p = set(['2p', '2a', '1a', '7p', '7a', '3p'])
+r2p = set(['1p', '1a', '2a', '4p'])
+r3p = set(['4p', '3a', '9p', '5p', '1p'])
+r4p = set(['4a', '3p', '3a', '2p', '6p'])
+r5p = set(['6p', '11p', '5a', '15as', '3p', '6p'])
+r6p = set(['5p', '6a', '5a', '4p'])
+r7p = set(['1p', '8p', '7a', '8a', '1a', '9p'])
+r8p = set(['7p', '8a', '10p', '7a'])
+r9p = set(['10p', '3p', '9a', '7a', '11p'])
+r10p = set(['9p', '10a', '9a', '8p', '12p'])
+r11p = set(['12p', '5p', '11a', '15as', '12a', '9p'])
+r12p = set(['11p', '12a', '11a', '10p'])
+r1a = set(['13as', '2a', '7a', '3a'])
+r2a = set(['1a', '13as', '2p', '4a'])
+r3a = set(['5a', '4p', '3p', '14as', '5a', '1p'])
+r4a = set(['3a', '14as', '4p', '2a', '6a'])
+r5a = set(['15as', '6a', '3a', '5p'])
+r6a = set(['5a', '15as', '6p', '4a'])
+r7a = set(['1a', '13as', '8a', '7p', '9a'])
+r8a = set(['8p', '7a', '13as', '10a'])
+r9a = set(['10a', '14as', '7a', '11a'])
+r10a = set(['10p', '8a', '12a', '9a'])
+r11a = set(['12a', '15as', '12p', '9a'])
+r12a = set(['11a', '15as', '12p', '10a'])
+r13as = set(['1a', '7a', '8a', '2a', '14as'])
+r14as = set(['3a', '9a', '4a', '10a', '13as', '15a'])
+r15as = set(['6a', '12a', '5a', '11a', '14as'])
