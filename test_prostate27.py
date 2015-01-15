9654d774ea0cc09771d3c307de163002f78572be
Mark Palmeri <mlp6@duke.edu>
2015-01-14 20:23:09 -0500

    test nn_ranges()

--- test_prostate27.py
+++ test_prostate27.py
@@ -8,6 +8,7 @@ class runTest(unittest.TestCase):
         p = Prostate27()
         self.assertIn('6a', p.nearest_neighbors('5a'))
         self.assertIn('11a', p.nearest_neighbors('15as'))
+        self.assertIn('6a', p.nearest_neighbors('15as'))
         self.assertIn('5p', p.nearest_neighbors('11p'))
 
     def test_location(self):
@@ -15,6 +16,12 @@ class runTest(unittest.TestCase):
         self.assertTrue(p.location('12a') == (2, 1, 3))
         self.assertTrue(p.location('1a') == (0, 1, 1))
 
+    def test_nn_ranges(self):
+        p = Prostate27()
+        self.assertTrue(p.nn_ranges((1, 2, 3)) ==
+                        [[0, 1, 2], [1, 2], [2, 3]])
+        self.assertTrue(p.nn_ranges((0, 0, 0)) ==
+                        [[0, 1], [0, 1], [0, 1, 2, 3]])
 
 if __name__ == '__main__':
     unittest.main()
