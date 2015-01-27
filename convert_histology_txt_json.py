58895ee6241099e29d9519554b2c389000cb2fdd
Mark Palmeri <mlp6@duke.edu>
2015-01-27 12:32:18 -0500

    make modular for function import

--- convert_histology_txt_json.py
+++ convert_histology_txt_json.py
@@ -1,40 +1,47 @@
-j = open('HistologyLesions.json', 'w')
-j.write('{\n')
+def main():
+    pass
 
-index = True
-benign = False
-with open('HistologyLesions.txt', 'r') as t:
-    tfile = t.readlines()
+def hist_txt_to_json():
+    j = open('HistologyLesions.json', 'w')
+    j.write('{\n')
 
-for td in tfile:
-    td = td[:-1]
+    index = True
+    benign = False
+    with open('HistologyLesions.txt', 'r') as t:
+        tfile = t.readlines()
 
-    if 'pca' in td and index:
-        j.write('\t"pca": [\n')
-        j.write('\t\t{\n\t\t\t"region": "%s",\n' % td.split(',')[1])
-        j.write('\t\t\t"volume_cc": %.1f,\n' % float(td.split(',')[2]))
-        j.write('\t\t\t"Gleason": %i,\n' % float(td.split(',')[3]))
-        j.write('\t\t\t"index": true\n\t\t}')
-        index = False
-    elif 'pca' in td and not index:
-        j.write(',\n')
-        j.write('\t\t{\n\t\t\t"region": "%s",\n' % td.split(',')[1])
-        j.write('\t\t\t"volume_cc": %.1f,\n' % float(td.split(',')[2]))
-        j.write('\t\t\t"Gleason": %i,\n' % float(td.split(',')[3]))
-        j.write('\t\t\t"index": false\n\t\t}')
+    for td in tfile:
+        td = td[:-1]
 
-    if ('atrophy' in td) or ('bph' in td):
-        if not benign:
-            j.write('],\n')
-        else:
+        if 'pca' in td and index:
+            j.write('\t"pca": [\n')
+            j.write('\t\t{\n\t\t\t"region": "%s",\n' % td.split(',')[1])
+            j.write('\t\t\t"volume_cc": %.1f,\n' % float(td.split(',')[2]))
+            j.write('\t\t\t"Gleason": %i,\n' % float(td.split(',')[3]))
+            j.write('\t\t\t"index": true\n\t\t}')
+            index = False
+        elif 'pca' in td and not index:
             j.write(',\n')
-        num_regions = len(td.split(',')[1:])
-        j.write('\t"%s": {\n\t\t"regions": [' % td.split(',')[0])
-        for n, r in enumerate(td.split(',')[1:]):
-            if n < (num_regions-1):
-                j.write('"%s", ' % r[1:])
-            else:
-                j.write('"%s"]\n\t\t}' % r[1:])
-        benign = True
+            j.write('\t\t{\n\t\t\t"region": "%s",\n' % td.split(',')[1])
+            j.write('\t\t\t"volume_cc": %.1f,\n' % float(td.split(',')[2]))
+            j.write('\t\t\t"Gleason": %i,\n' % float(td.split(',')[3]))
+            j.write('\t\t\t"index": false\n\t\t}')
 
-j.write('\n}')
+        if ('atrophy' in td) or ('bph' in td):
+            if not benign:
+                j.write('],\n')
+            else:
+                j.write(',\n')
+            num_regions = len(td.split(',')[1:])
+            j.write('\t"%s": {\n\t\t"regions": [' % td.split(',')[0])
+            for n, r in enumerate(td.split(',')[1:]):
+                if n < (num_regions-1):
+                    j.write('"%s", ' % r[1:])
+                else:
+                    j.write('"%s"]\n\t\t}' % r[1:])
+            benign = True
+
+    j.write('\n}')
+
+if __name__ == '__main__':
+    main()
