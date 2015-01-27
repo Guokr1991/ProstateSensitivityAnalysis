5a85d55d685e9a039108764aca0c44f1a8a740b6
Mark Palmeri <mlp6@duke.edu>
2015-01-27 15:10:35 -0500

    fix histology path bug

--- convert_all_txt_json.py
+++ convert_all_txt_json.py
@@ -5,7 +5,7 @@ from convert_arfi_txt_json import arfi_txt_to_json
 for p in range(56, 107):
     d = '/luscinia/ProstateStudy/invivo/Patient%i' % p
     if os.path.exists('%s/Histology/HistologyLesions.txt' % d):
-        os.chdir(d)
+        os.chdir('%s/Histology/' % d)
         print "Histology: Converting %s" % d
         hist_txt_to_json()
     else:
