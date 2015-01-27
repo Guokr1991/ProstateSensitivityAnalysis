dc5deec95d793e82d0f200c38f50aab9d1b53e32
Mark Palmeri <mlp6@duke.edu>
2015-01-27 15:05:47 -0500

    correct error if only pca and no benign lesions

--- convert_histology_txt_json.py
+++ convert_histology_txt_json.py
@@ -40,9 +40,8 @@ def hist_txt_to_json():
                 else:
                     j.write('"%s"]\n\t\t}' % r[1:])
             benign = True
-    
-    if not benign: # end pca vector if no benign confounders
-        j.write(']\n')
+        else:
+            j.write(']\n')
 
     j.write('\n}')
 
