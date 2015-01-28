10b3d203fafce7d6bc6580740a51ff8f6a595f12
Mark Palmeri <mlp6@duke.edu>
2015-01-27 19:22:33 -0500

    pass clinical significance test

--- test_lesion_analysis.py
+++ test_lesion_analysis.py
@@ -4,15 +4,15 @@ from lesion_analysis import LesionAnalysis
 
 class runTest(unittest.TestCase):
 
-#    def test_clin_sig(self):
-#        """
-#        histology clinical significance
-#        """
-#        self.assertTrue(LesionAnalysis.clin_sig(100, 8))
-#        self.assertFalse(LesionAnalysis.clin_sig(100, 5))
-#        self.assertFalse(LesionAnalysis.clin_sig(100, 6))
-#        self.assertTrue(LesionAnalysis.clin_sig(700, 6))
-#
+    def test_clin_sig(self):
+        """
+        histology clinical significance
+        """
+        self.assertTrue(LesionAnalysis.clin_sig(100, 8))
+        self.assertFalse(LesionAnalysis.clin_sig(100, 5))
+        self.assertFalse(LesionAnalysis.clin_sig(100, 6))
+        self.assertTrue(LesionAnalysis.clin_sig(700, 6))
+
     def test_no_index_match(self):
         """
         no histology index lesion
