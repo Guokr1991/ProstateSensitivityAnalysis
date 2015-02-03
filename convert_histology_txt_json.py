90c233806f4d700aff2e81101e022064f789a81f
Tyler Glass <tylerglass33@gmail.com>
2015-02-03 14:32:19 -0500

    Syntax error

--- convert_histology_txt_json.py
+++ convert_histology_txt_json.py
@@ -42,7 +42,7 @@ def hist_txt_to_json():
             benign = True
     
     if not benign: # end pca vector if no benign confounders
-        j.write('],\n')
+        j.write(']\n')
 
     j.write('\n}')
 
