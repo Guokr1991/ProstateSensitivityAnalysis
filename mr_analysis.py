17d9e4e7d27d3bbd0495210753fcd5f9c0cbe25e
Tyler Glass <tylerglass33@gmail.com>
2015-02-09 16:50:34 -0500

    Updated MRAnalysis class for JSON input

--- mr_analysis.py
+++ mr_analysis.py
@@ -12,78 +12,50 @@ class MRAnalysis(LesionAnalysis):
     def __init__(self, pnum, root='/luscinia/ProstateStudy/invivo'):
         self.pnum = pnum
         self.root = '%s/Patient%s' % (root, self.pnum)
-        # self.root = '/home/mlp6/Downloads/invivo/Patient%s' % self.pnum
-        self.mri_ios = '%s/MRI_Images/MRI_Index_Lesion.txt' % self.root
-        self.hist_lesions = '%s/Histology/HistologyLesions.txt' % self.root
-        self.read_mri()
-        self.arfi = self.mri  # defined so self.arfi points to self.mri for lesion analysis of MRI
-        self.read_histology()
-        self.valid_dataset()
+        self.histology = self.read_json('%s/Histology/HistologyLesions.json' %
+                                        self.root)
+        self.arfi = self.read_json('%s/ARFI_Lesions.json' % self.root) # read in arfi
+        self.mri = self.read_json('%s/MRI_Images/MRILesions.json' % self.root) # read in mri
+        self.valid = self.valid_dataset(self.mri, self.histology)
         if self.valid:
             self.mri_lesions()
+            self.arfi_lesions() # keep arfi analysis in MRanalysis class
             self.histology_lesions()
             self.check_index_match()
             self.check_benign_match()
             self.check_clin_sig_match()
             self.check_hist_clin_sig_sensitivity()
-            
-    def read_mri(self):
-        """
-        read MRI lesion IOS data
-        """
-        self.mri = {}
-        read_index ={} #read MRI index here so not out of order
-        
-        try:
-            with open(self.mri_ios, 'r') as f:
-                mriios = f.readlines()
-
-            if 'None' not in mriios[0]:
-                for lesion in mriios:
-                    read_index['region']=lesion.split(', ')[0]
-                    break
-                for lesion in mriios:
-                    lesion = lesion[:-1]
-                    self.mri[lesion.split(', ')[0]] = lesion.split(', ')[1:]
-                self.mri['index'] = read_index   
-            else:
-                self.mri['read'] = 'no lesions read'
-        except IOError:
-            # print "%s does not exist" % self.mri_ios
-            self.mri[None] = None
-                      
+                                 
     def mri_lesions(self):
         """
-        characterize all MRI lesions and define MRI index lesion dict based on
-        highest IOS
+        characterize all MRI lesions and define MRI index lesion
         """
         if self.mri.values()[0] != 'no lesions read':
             try:
                 from prostate27 import Prostate27
-                prostate = Prostate27()
+                p = Prostate27()
 
                 index = {}
-                index['IOS']      = max(self.mri.values())[0] # extract IOS only
-                index['gleason']  = max(self.mri.values())[1]
-                index['diameter'] = max(self.mri.values())[2]
-                index['extracap'] = max(self.mri.values())[3]
-                index['region'] = self.mri['index']['region']
-                index['location'] = prostate.anterior_posterior(index['region'])
-                index['zone'] = prostate.zone(index['region'])
-                self.mri['index'] = index
+                for lesion in self.mri['lesions']:
+                    if lesion['index'] is True:
+                        index['IOS'] = lesion['IOS']
+                        index['region'] = lesion['region']
+                        index['location'] = \
+                            p.anterior_posterior([lesion['region']])
+                        index['zone'] = p.zone(lesion['region'])
+                        index['Gleason'] = lesion['Gleason']
+                        index['diameter_mm'] = lesion['diameter_mm']
+                        index['ECE'] = lesion['extracap']
+                        index['lesion_number'] = lesion['lesion_number']
+                        self.mri['index'] = index
+                        break
             except ValueError:
                 self.mri['index'] = None
 
             # specify location and zone for each lesion
-            for lesion in self.mri:
-                if 'index' not in lesion:
-                    self.mri[lesion] = {'IOS': self.mri["%s"      % lesion][0],
-                                        'gleason': self.mri["%s"  % lesion][1],
-                                        'diameter': self.mri["%s" % lesion][2],
-                                        'extracap': self.mri["%s" % lesion][3],
-                                        'location':
-                                         prostate.anterior_posterior(lesion),
-                                         'zone': prostate.zone(lesion)}
+            #for n, lesion in enumerate(self.mri['lesions']):
+            #    self.mri['lesions'][n].update({'location': p.anterior_posterior([lesion['region']]),
+             #                                   'zone': p.zone(lesion['region'])})                                     'zone': prostate.zone(lesion)}
     def __str__(self):
         """
         print analysis results
