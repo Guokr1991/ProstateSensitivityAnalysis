90bff91ae67db8994da131388c12b47a50e5dd95
Tyler Glass <tylerglass33@gmail.com>
2015-02-10 15:38:01 -0500

    Added MRI/path JSON test cases as comments, need to uncomment after merge MRI_lesion_analysis branch

--- test_lesion_analysis.py
+++ test_lesion_analysis.py
@@ -1,5 +1,6 @@
 import unittest
 from lesion_analysis import LesionAnalysis
+from mr_analysis import MRAnalysis
 
 
 class runTest(unittest.TestCase):
@@ -66,6 +67,126 @@ class runTest(unittest.TestCase):
         P12 = LesionAnalysis(12, './testing')
         self.assertFalse(P12.benign_match['bph'])
         self.assertTrue(P12.benign_match['atrophy'])
-
+    
+#    def test_histology_ECE(self):
+#        """
+#        Histology JSON ECE parsing
+#        """
+#        P10 = LesionAnalysis(10, './testing')
+#        self.assertTrue(P10.histology['index']['Staging']='T3a')
+#        self.assertTrue(P10.histology['index']['ECE_extent']='Established')
+#        P10 = MRAnalysis(10, './testing') 
+#        self.assertTrue(P10.histology['index']['Staging']='T3a')
+#        self.assertTrue(P10.histology['index']['ECE_extent']='Established')  
+#        P13 = MRAnalysis(13, './testing') 
+#        self.assertTrue(P13.histology['index']['Staging']='T2x')
+#        self.assertTrue(P13.histology['index']['ECE_extent']='None')                 
+#        
+#    def test_valid_MR_dataset(self):
+#        """#    def test_valid_MR_dataset(self):
+#        """
+#        valid MRI dataset
+#        """
+#        P13 = MRAnalysis(13, './testing')
+#        self.assertFalse(P13.valid)
+#
+#        P11 = MRAnalysis(11, './testing')
+#        self.assertTrue(P11.valid)
+#
+#        P14 = MRAnalysis(14, './testing')
+#        self.assertTrue(P14.valid)
+#
+#    def test_mri_lesion(self):
+#        """
+#        MRI file parsing for index lesion
+#        """
+#        P11 = MRAnalysis(11, './testing')
+#
+#
+#        self.assertTrue(P11.mri['index']['region'] == '9p')
+#        self.assertTrue(P11.mri['index']['IOS'] == 3)
+#        # these tests depend on Prostate27, but I'll include them too
+#        self.assertTrue(P11.mri['index']['location'] == 'posterior')
+#        self.assertTrue(P11.mri['index']['zone'] == 'peripheral zone')
+#        # these tests are only relevant for MRI data
+#        self.assertTrue(P11.mri['index']['Gleason'] == 7)
+#        self.assertTrue(P11.mri['index']['diameter_mm'] == 8)
+#        self.assertTrue(P11.mri['index']['ECE'] == false)
+#        self.assertTrue(P11.mri['index']['lesion_number'] == 1)
+#        # test for case with multiple IOS3 lesions (top line in text file is index)
+#        P14 = MRAnalysis(14, './testing')
+#        self.assertTrue(P14.mri['index']['region'] == '10p')
+#        self.assertTrue(P14.mri['index']['IOS'] == 3)
+#        self.assertFalse(P14.mri['index']['region'] == '3p')
+#
+#    def test_MR_no_index_match(self):
+#        """
+#        no histology index lesion using MRAnalysis class
+#        """
+#        P13 = MRAnalysis(13, './testing')
+#        self.assertFalse(P13.check_index_match())
+#
+#    def test_histology_mri_index_match(self):
+#        """
+#        histology / MRI index match
+#        """
+#        P11 = MRAnalysis(11, './testing')
+#        self.assertFalse(P11.index_match['exact'])
+#        self.assertTrue(P11.index_match['nn'])
+#
+#        P14 = MRAnalysis(14, './testing')
+#        self.assertFalse(P14.index_match['exact'])
+#        self.assertTrue(P14.index_match['nn'])
+#        valid MRI dataset
+#        """
+#        P13 = MRAnalysis(13, './testing')
+#        self.assertFalse(P13.valid)
+#
+#        P11 = MRAnalysis(11, './testing')
+#        self.assertTrue(P11.valid)
+#
+#        P14 = MRAnalysis(14, './testing')
+#        self.assertTrue(P14.valid)
+#
+#    def test_mri_lesion(self):
+#        """
+#        MRI file parsing for index lesion
+#        """
+#        P11 = MRAnalysis(11, './testing')
+#        self.assertTrue(P11.mri['index']['region'] == '9p')
+#        self.assertTrue(P11.mri['index']['IOS'] == 3)
+#        # these tests depend on Prostate27, but I'll include them too
+#        self.assertTrue(P11.mri['index']['location'] == 'posterior')
+#        self.assertTrue(P11.mri['index']['zone'] == 'peripheral zone')
+#        # these tests are only relevant for MRI data
+#        self.assertTrue(P11.mri['index']['Gleason'] == 7)
+#        self.assertTrue(P11.mri['index']['diameter_mm'] == 8)
+#        self.assertTrue(P11.mri['index']['ECE'] == false)
+#        self.assertTrue(P11.mri['index']['lesion_number'] == 1)
+#        # test for case with multiple IOS3 lesions (top line in text file is index)
+#        P14 = MRAnalysis(14, './testing')
+#        self.assertTrue(P14.mri['index']['region'] == '10p')
+#        self.assertTrue(P14.mri['index']['IOS'] == 3)
+#        self.assertFalse(P14.mri['index']['region'] == '3p')
+#
+#    def test_MR_no_index_match(self):
+#        """
+#        no histology index lesion using MRAnalysis class
+#        """
+#        P13 = MRAnalysis(13, './testing')
+#        self.assertFalse(P13.check_index_match())
+#
+#    def test_histology_mri_index_match(self):
+#        """
+#        histology / MRI index match
+#        """
+#        P11 = MRAnalysis(11, './testing')
+#        self.assertFalse(P11.index_match['exact'])
+#        self.assertTrue(P11.index_match['nn'])
+#
+#        P14 = MRAnalysis(14, './testing')
+#        self.assertFalse(P14.index_match['exact'])
+#        self.assertTrue(P14.index_match['nn'])
+  
 if __name__ == '__main__':
     unittest.main()
